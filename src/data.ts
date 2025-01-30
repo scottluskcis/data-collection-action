1f8d4543b3dfe06a8be7894f8e33cd482d1f3f69
Scott Lusk <scottlusk+es202412@0t2lt.onmicrosoft.com>
2025-01-30 19:03:11 +0000

    add in code to call endpoints

--- src/data.ts
+++ src/data.ts
@@ -2,13 +2,43 @@ import fs from 'fs'
 import { Octokit } from '@octokit/rest'
 import { retry } from '@octokit/plugin-retry'
 import { throttling } from '@octokit/plugin-throttling'
+import { components } from '@octokit/openapi-types'
+
+interface Webhook {
+  name: string
+  url: string
+  active: boolean
+  last_response: {
+    code: number | null
+    status: string | null
+    message: string | null
+  }
+}
+
+interface RepoStats {
+  org: string
+  name: string
+  archived: boolean
+  created_at: string | null
+  pushed_at: string | null
+  updated_at: string | null
+  runners: number | null
+  secrets: number | null
+  variables: number | null
+  environments: number | null | undefined
+  hooks: Webhook[] | null
+}
+
+type RepoType = components['schemas']['repository']
+
+//const IGNORED_ORGS = ['github', 'actions']
 
 const _Octokit = Octokit.plugin(retry, throttling)
 
-async function newClient(token: string): Promise<Octokit> {
+async function newClient(url: string, token: string): Promise<Octokit> {
   return new _Octokit({
     auth: token,
-    baseUrl: process.env.GHES_URL,
+    //baseUrl: url,
     retries: 10,
     throttle: {
       onRateLimit: (retryAfter, options, octokit) => {
@@ -33,11 +63,9 @@ async function newClient(token: string): Promise<Octokit> {
   })
 }
 
-const IGNORED_ORGS = ['github', 'actions']
-
-const getRunnerCount = async (client: Octokit, org: string, repo: string) => {
+const getRunnerCount = async (octokit: Octokit, org: string, repo: string) => {
   try {
-    const { data: runners } = await client.request(
+    const { data: runners } = await octokit.request(
       'GET /repos/{owner}/{repo}/actions/runners',
       {
         owner: org,
@@ -53,9 +81,9 @@ const getRunnerCount = async (client: Octokit, org: string, repo: string) => {
   }
 }
 
-const getSecretsCount = async (client: Octokit, org: string, repo: string) => {
+const getSecretsCount = async (octokit: Octokit, org: string, repo: string) => {
   try {
-    const { data: secrets } = await client.request(
+    const { data: secrets } = await octokit.request(
       'GET /repos/{owner}/{repo}/actions/secrets',
       {
         owner: org,
@@ -72,12 +100,12 @@ const getSecretsCount = async (client: Octokit, org: string, repo: string) => {
 }
 
 const getVariablesCount = async (
-  client: Octokit,
+  octokit: Octokit,
   org: string,
   repo: string
 ) => {
   try {
-    const { data: variables } = await client.request(
+    const { data: variables } = await octokit.request(
       'GET /repos/{owner}/{repo}/actions/variables',
       {
         owner: org,
@@ -94,12 +122,12 @@ const getVariablesCount = async (
 }
 
 const getEnvironmentsCount = async (
-  client: Octokit,
+  octokit: Octokit,
   org: string,
   repo: string
 ) => {
   try {
-    const { data: environments } = await client.request(
+    const { data: environments } = await octokit.request(
       'GET /repos/{owner}/{repo}/environments',
       {
         owner: org,
@@ -115,9 +143,13 @@ const getEnvironmentsCount = async (
   }
 }
 
-const webhooks = async (client: Octokit, org: string, repo: string) => {
+const webhooks = async (
+  octokit: Octokit,
+  org: string,
+  repo: string
+): Promise<Webhook[] | null> => {
   try {
-    const webhooks = await client.paginate('GET /repos/{owner}/{repo}/hooks', {
+    const webhooks = await octokit.paginate('GET /repos/{owner}/{repo}/hooks', {
       owner: org,
       repo: repo,
       per_page: 100
@@ -125,9 +157,13 @@ const webhooks = async (client: Octokit, org: string, repo: string) => {
     return webhooks.map((hook) => {
       return {
         name: hook.name,
-        url: hook.config.url,
+        url: hook.config.url || '',
         active: hook.active,
-        last_response: hook.last_response
+        last_response: {
+          code: hook.last_response.code,
+          status: hook.last_response.status,
+          message: hook.last_response.message
+        }
       }
     })
   } catch (e: unknown) {
@@ -137,23 +173,94 @@ const webhooks = async (client: Octokit, org: string, repo: string) => {
   }
 }
 
-export async function collectData() {
+const getRepoStats = async (
+  octokit: Octokit,
+  org: string,
+  repo: RepoType
+): Promise<RepoStats> => {
+  const name = repo.name
+
+  const [runners, secrets, variables, environments, hooks] = await Promise.all([
+    getRunnerCount(octokit, org, name),
+    getSecretsCount(octokit, org, name),
+    getVariablesCount(octokit, org, name),
+    getEnvironmentsCount(octokit, org, name),
+    webhooks(octokit, org, name)
+  ])
+
+  const result: RepoStats = {
+    org: org,
+    name: name,
+    archived: repo.archived,
+    created_at: repo.created_at,
+    pushed_at: repo.pushed_at,
+    updated_at: repo.updated_at,
+    runners: runners,
+    secrets: secrets,
+    variables: variables,
+    environments: environments,
+    hooks: hooks
+  }
+  return result
+}
+export async function collectData({
+  url,
+  token,
+  org
+}: {
+  url: string
+  token: string
+  org: string
+}): Promise<void> {
   const results = []
 
-  const token = process.env.GHES_TOKEN
-  if (!token) {
-    console.error('GHES_TOKEN environment variable is required')
-    return
-  }
-
-  const client = await newClient(token)
+  const octokit = await newClient(url, token)
+  /*
   const _orgs = await client.paginate('GET /organizations', {
     per_page: 100
   })
   const orgs = _orgs
     .map((org) => org.login)
     .filter((org) => !IGNORED_ORGS.includes(org))
+    */
+  const orgs = [org]
   for (const org of orgs) {
+    const _repos = octokit.paginate.iterator(octokit.rest.repos.listForOrg, {
+      org: org,
+      per_page: 100
+    })
+
+    for await (const { data: repos } of _repos) {
+      for (const repo of repos) {
+        const result = await getRepoStats(octokit, org, repo as RepoType)
+
+        /*
+        const name = repo.name
+        const runners = await getRunnerCount(octokit, org, name)
+        const secrets = await getSecretsCount(octokit, org, name)
+        const variables = await getVariablesCount(octokit, org, name)
+        const environments = await getEnvironmentsCount(octokit, org, name)
+        const hooks = await webhooks(octokit, org, name)
+        const result = {
+          org: org,
+          name: name,
+          archived: repo.archived,
+          created_at: repo.created_at,
+          pushed_at: repo.pushed_at,
+          updated_at: repo.updated_at,
+          runners: runners,
+          secrets: secrets,
+          variables: variables,
+          environments: environments,
+          hooks: hooks
+        }
+        console.log(JSON.stringify(result))
+        */
+        console.log(JSON.stringify(result))
+        results.push(result)
+      }
+    }
+    /*
     const repos = await client.paginate('GET /orgs/{org}/repos', {
       org: org,
       per_page: 100
@@ -181,6 +288,7 @@ export async function collectData() {
       console.log(JSON.stringify(result))
       results.push(result)
     }
+    */
   }
   fs.writeFileSync('/tmp/results.json', JSON.stringify(results))
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+//import { wait } from './wait.js'
+import { collectData } from './data.js'
 
 /**
  * The main function for the action.
@@ -10,12 +11,28 @@ export async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
 
+    const url = process.env.GH_SOURCE_URL
+    if (!url) {
+      throw new Error('GH_SOURCE_URL is not set')
+    }
+
+    const token = process.env.GH_SOURCE_TOKEN
+    if (!token) {
+      throw new Error('GH_SOURCE_TOKEN is not set')
+    }
+
+    const org = process.env.GH_SOURCE_ORG
+    if (!org) {
+      throw new Error('GH_SOURCE_ORG is not set')
+    }
+
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    await collectData({ url, token, org })
+    //await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
